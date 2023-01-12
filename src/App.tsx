a24bb8d61ab9009b7e9234defba103ee95f6a2e7
YoonYoungChae <YoonYoungChae>
2023-01-12 13:58:10 +0900

    수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ import "./App.css";
 import { fileUploadApi } from "./apiservice/api";
 
 function App() {
+    const [file, setFile] = React.useState(null);
+    const [formDataList, setFormDataList] = React.useState<FormData>();
     const fileApi = async (file: any) => {
         const res = await fileUploadApi(file);
         console.log("file api response : ", res);
     };
     const fileChange = (e: any) => {
-        const formData = new FormData();
-        formData.append("file", e.target.files[0]);
-
-        fileApi(formData);
+        let formData = new FormData();
+        for (let file of e.target.files) {
+            formData.append("files", file);
+        }
+        setFormDataList(formData);
     };
+
+    React.useEffect(() => {
+        if (formDataList !== undefined) {
+            fileApi(formDataList);
+        }
+    }, [formDataList]);
+
     return (
         <div className="App">
             <input type="file" multiple={true} id="fileUpload" onChange={fileChange}></input>
